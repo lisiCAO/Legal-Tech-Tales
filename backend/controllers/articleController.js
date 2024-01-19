4dd6e5725716a3c1d0f0fb5a56b88a995122d46b
lisiCAO <caolisi@hotmail.com>
2024-01-19 14:12:19 -0500

    create frame

---
+++ backend/controllers/articleController.js
@@ -0,0 +1,13 @@
+const Article = require('../models/article');
+
+exports.addArticle = async (req, res) => {
+  // TODO: add article
+};
+
+exports.viewArticle = async (req, res) => {
+  // TODO: view article
+};
+
+exports.listArticles = async (req, res) => {
+  // TODO: list articles
+};
---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+exports.register = async (req, res) => {
+  // TODO: register user
+};
+
+exports.login = async (req, res) => {
+  // TODO: login user
+};
