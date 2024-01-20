82f1a5470984fd44ac5e43f3802a87992d46a153
lisiCAO <caolisi@hotmail.com>
2024-01-20 09:23:41 -0500

    delete comments

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -2,27 +2,21 @@ const Article = require('../models/article');
 
 exports.addArticle = async (req, res) => {
   try {
-    // 从请求体中提取文章数据
     const { title, body } = req.body;
 
-    // 假设用户已通过认证中间件认证，并且他们的ID存储在req.user.userId
     const newArticle = new Article({
-      authorId: req.user.userId, // 用户ID从认证信息中获取
+      authorId: req.user.userId, 
       title: title,
       body: body
     });
 
-    // 保存新文章到数据库
     const savedArticle = await newArticle.save();
 
-    // 发送成功响应
     res.status(201).send(savedArticle);
   } catch (error) {
-    // 捕获并处理来自Mongoose模型的验证错误
     if (error.name === 'ValidationError') {
       return res.status(400).send(error.message);
     }
-    // 处理其他潜在的服务器错误
     res.status(500).send('Error adding article');
   }
 };
