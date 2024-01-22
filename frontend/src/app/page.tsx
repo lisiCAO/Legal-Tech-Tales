0d87b1a1ee43991810f4f36731869424ed61e076
lisiCAO <caolisi@hotmail.com>
2024-01-22 18:14:31 -0500

    import image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
-
+import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-6 bg-custom-cream">
       <div className="w-full max-w-[960px] mx-auto flex justify-center">
-        <img src="/banner.jpg" alt="Blog Banner" className="w-auto" />
+        <Image
+          src="/path/to/image.jpg"
+          alt="description"
+          width={500}
+          height={300}
+          layout="responsive"
+        />
       </div>
       <div className="text-center">
         <h1 className="text-6xl font-bold text-custom-darkorange mb-6">Welcome to my Blog</h1>
